13c93270917d82df05def46b0cb3fe04a6bc5c82
Nhan Bui <buithiennhan0345@gmail.com>
2024-05-29 17:48:36 +0700

    update authMiddleware

--- src/app/middlewares/rolesMiddleware.js
+++ src/app/middlewares/rolesMiddleware.js
@@ -1,9 +1,19 @@
-const { UserModel } = require('../models');
+const { UserModel, RoleModel } = require('../models');
 const AuthorizeError = require('../errors/AuthorizeError');
+const NotFoundError = require('../errors/NotFoundError');
 
 const adminAuth = async (req, res, next) => {
+  const adminAuth = await RoleModel.findOne({
+    where: {
+      name: 'Admin',
+    },
+  });
+
+  if (adminAuth === null) throw new NotFoundError({ message: 'Not found admin role' });
+
   const userId = req.userId;
-  const user = await UserModel.findOne({ where: { id: userId, roleId: 1 } });
+  const user = await UserModel.findOne({ where: { id: userId, roleId: adminAuth.id } });
+
   if (user === null) throw new AuthorizeError({ message: 'Must be admin authorization' });
   next();
 };
