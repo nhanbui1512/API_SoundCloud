50a12f281fb0da60a92a57632c7932ac9a601f29
nhanbui1512 <buithiennhan0345@gmail.com>
2024-06-14 07:24:05 +0700

    change api route of login

--- src/app/routes/index.js
+++ src/app/routes/index.js
@@ -2,7 +2,6 @@ const homeRouter = require('./homeRoute');
 const genreRouter = require('./genreRoute');
 const songRouter = require('./songRoute');
 const userRouter = require('./userRoute');
-const loginRouter = require('./loginRoute');
 const listenRouter = require('./listenRoute');
 const followRouter = require('./followRoute');
 const playlistRouter = require('./playlistRoute');
@@ -25,7 +24,6 @@ function route(app) {
   app.use('/api/genre', genreRouter);
   app.use('/api/song', songRouter);
   app.use('/api/user', userRouter);
-  app.use('/api/login', loginRouter);
   app.use('/api/listen', listenRouter);
   app.use('/api/follow', followRouter);
   app.use('/api/playlist', playlistRouter);
