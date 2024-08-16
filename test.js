f72cecc40a303b72c89e297ffe14caa1bd3807fd
nhanbui1512 <buithiennhan0345@gmail.com>
2024-08-16 21:56:52 +0700

    update

--- test.js
+++
@@ -1,5 +0,0 @@
-var str = `http://res.cloudinary.com/dmykkmqwz/video/upload/v1723372365/audios/cd9vk3clyzat8eitd1rw.mp3`;
-const startIndex = str.indexOf('audios');
-const splited = str.substring(startIndex, str.length);
-
-console.log(splited);
